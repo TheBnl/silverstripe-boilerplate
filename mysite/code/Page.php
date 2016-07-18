61c4d36ffc062ec118f37784cce036887e233e22
Bram de Leeuw <bram-de-leeuw@hotmail.com>
2016-07-18 09:30:58 +0200

    added spam protection modules and a default robots.txt

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -7,7 +7,7 @@ class Page extends SiteTree {
 	private static $db = array();
 
 	private static $has_one = array(
-			"OpenGraphImage" => "Image"
+			'OpenGraphImage' => 'Image'
 	);
 
 	private static $has_many = array();
@@ -23,21 +23,21 @@ class Page extends SiteTree {
 
 	public function getCMSFields() {
 		$this->beforeUpdateCMSFields(function($fields) {
-			$openGraphImage = new UploadField("OpenGraphImage", "Social media image");
-			$openGraphImage->setDescription("Add an image to display on Facebook and Twitter");
+			$openGraphImage = new UploadField('OpenGraphImage', 'Social media image');
+			$openGraphImage->setDescription('Add an image to display on Facebook and Twitter');
 
-			$fields->addFieldToTab("Root.Main.Metadata", $openGraphImage);
+			$fields->addFieldToTab('Root.Main.Metadata', $openGraphImage);
 		});
 
 		$fields = parent::getCMSFields();
-		$fields->removeByName(array("ExtraMeta"));
+		$fields->removeByName(array('ExtraMeta'));
 		return $fields;
 	}
 
 	public function getSettingsFields() {
 		$fields = parent::getSettingsFields();
-		// Hide ShowInSearch checkbox if we don"t have a search
-		$fields->removeByName("ShowInSearch");
+		// Hide ShowInSearch checkbox if we don't have a search
+		$fields->removeByName('ShowInSearch');
 		return $fields;
 	}
 
@@ -66,31 +66,31 @@ class Page_Controller extends ContentController {
 	public function init() {
 		parent::init();
 
-		require_once Director::baseFolder() ."/vendor/mobiledetect/mobiledetectlib/Mobile_Detect.php";
+		require_once Director::baseFolder() .'/vendor/mobiledetect/mobiledetectlib/Mobile_Detect.php';
 		$detect = new Mobile_Detect;
 
-		Config::inst()->update("Page", "is_mobile", $detect->isMobile());
-		Config::inst()->update("Page", "is_phone", $detect->isMobile() && !$detect->isTablet());
-		Config::inst()->update("Page", "is_tablet", $detect->isTablet());
+		Config::inst()->update('Page', 'is_mobile', $detect->isMobile());
+		Config::inst()->update('Page', 'is_phone', $detect->isMobile() && !$detect->isTablet());
+		Config::inst()->update('Page', 'is_tablet', $detect->isTablet());
 
-		Requirements::set_combined_files_folder(project() . "/_combinedfiles");
-		Requirements::combine_files("app.js", array(
-			PROJECT_THIRDPARTY_DIR . "/jquery/dist/jquery.min.js",
-			PROJECT_THIRDPARTY_DIR . "/lazyloadxt/dist/jquery.lazyloadxt.min.js",
-			PROJECT_THIRDPARTY_DIR . "/Swiper/dist/js/swiper.jquery.min.js",
-			project() . "/javascript/app.js",
+		Requirements::set_combined_files_folder(project() . '/_combinedfiles');
+		Requirements::combine_files('app.js', array(
+			PROJECT_THIRDPARTY_DIR . '/jquery/dist/jquery.min.js',
+			PROJECT_THIRDPARTY_DIR . '/lazyloadxt/dist/jquery.lazyloadxt.min.js',
+			PROJECT_THIRDPARTY_DIR . '/Swiper/dist/js/swiper.jquery.min.js',
+			project() . '/javascript/app.js',
 		));
 		
 		Requirements::insertHeadTags(sprintf(
-			"<script src='%s'></script>", PROJECT_THIRDPARTY_DIR . "/modernizr/modernizr.min.js"
+			"<script src='%s'></script>", PROJECT_THIRDPARTY_DIR . '/modernizr/modernizr.min.js'
 		));
 
 //		Requirements::insertHeadTags(sprintf(
-//			"<script src='https://use.typekit.net/%s.js'></script><script>try{Typekit.load({ async: true });}catch(e){}</script>", "TYPEKIT_ID"
+//			'<script src='https://use.typekit.net/%s.js'></script><script>try{Typekit.load({ async: true });}catch(e){}</script>', 'TYPEKIT_ID'
 //		));
 
 //		Requirements::insertHeadTags(sprintf(
-//			"<script>
+//			'<script>
 //				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 //				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 //				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -99,14 +99,14 @@ class Page_Controller extends ContentController {
 //				ga('create', '%s', 'auto');
 //				ga('send', 'pageview');
 //
-//			</script>",
-//			"UA-XXXXX-X"
+//			</script>',
+//			'UA-XXXXX-X'
 //		));
 
-		Requirements::combine_files("app.css", array(
+		Requirements::combine_files('app.css', array(
 			// include any javascript library css like this
-			PROJECT_THIRDPARTY_DIR . "/Swiper/dist/css/swiper.min.css",
-			project() . "/css/app.css"
+			PROJECT_THIRDPARTY_DIR . '/Swiper/dist/css/swiper.min.css',
+			project() . '/css/app.css'
 		));
 	}
 
@@ -115,7 +115,7 @@ class Page_Controller extends ContentController {
 	 * @return array|scalar
 	 */
 	public function IsMobile() {
-		return Config::inst()->get("Page", "is_mobile");
+		return Config::inst()->get('Page', 'is_mobile');
 	}
 
 	/**
@@ -123,7 +123,7 @@ class Page_Controller extends ContentController {
 	 * @return array|scalar
 	 */
 	public function IsPhone() {
-		return Config::inst()->get("Page", "is_phone");
+		return Config::inst()->get('Page', 'is_phone');
 	}
 
 	/**
@@ -131,7 +131,7 @@ class Page_Controller extends ContentController {
 	 * @return array|scalar
 	 */
 	public function IsTablet() {
-		return Config::inst()->get("Page", "is_tablet");
+		return Config::inst()->get('Page', 'is_tablet');
 	}
 
 	/**
