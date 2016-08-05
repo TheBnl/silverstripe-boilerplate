caa252993a5ca9ba774f0e5440cc2a44415ef921
Bram de Leeuw <bram-de-leeuw@hotmail.com>
2016-08-05 16:53:31 +0200

    Added fontawesome cdn implementation

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -90,6 +90,10 @@ class Page_Controller extends ContentController {
 //		));
 
 //		Requirements::insertHeadTags(sprintf(
+//			"<script src='https://use.fontawesome.com/%s.js'></script>", 'FONTAWESOME_ID'
+//		));
+
+//		Requirements::insertHeadTags(sprintf(
 //			'<script>
 //				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 //				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
