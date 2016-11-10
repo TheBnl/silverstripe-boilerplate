754baaddcd0e63b2a5fe0f82571b55e5c4be22f4
Mike <mike.zonnevijlle@gmail.com>
2016-11-10 14:35:19 +0100

    Update gulp and structure (#5)
    * Create file.js
    
    * Rename mysite/javascript/app.js to mysite/javascript/app/app.js
    
    * Update bundle.js
    
    * Update gulpfile.js
    
    * Update README.md

--- mysite/gulpfile.js
+++ mysite/gulpfile.js
@@ -5,53 +5,54 @@ const uglify = require('gulp-uglify');
 const pump = require('pump');
 const babel = require('gulp-babel');
 const bundle = require('./javascript/bundle.js');
-const sass = require('gulp-sass');
+var sass = require('gulp-sass');
 
 gulp.task('default', function(cb) {
-    return doBuild(cb);
+	return doBuild(cb);
 });
 
 gulp.task('quickBuild', function (cb) {
-    return doWatch(cb);
+	return doWatch(cb);
 });
 
 gulp.task('sass', function (cb) {
-    return doSass(cb);
+	return doSass(cb);
 });
 
 gulp.task('watch', ['quickBuild','sass'], function(cb) {
-    gulp.watch('javascript/*.js', ['quickBuild']);
-    gulp.watch('scss/**/*.scss', ['sass']);
+	gulp.watch('javascript/app/**/*.js', ['quickBuild']);
+	gulp.watch('scss/**/*.scss', ['sass']);
 });
 
 function doBuild(cb) {
-    gutil.log('compiling scripts');
-    pump([
-        gulp.src(bundle.BUNDLE),
-        babel({
-            presets: ['es2015']
-        }),
-        concat('bundle.js'),
-        uglify(),
-        gulp.dest('javascript/dist')
-    ], cb);
+	gutil.log('compiling scripts',bundle.BUNDLE);
+	pump([
+		gulp.src(bundle.BUNDLE),
+		babel({
+			presets: ['es2015']
+		}),
+		concat('bundle.js'),
+		uglify(),
+		gulp.dest('javascript/dist')
+	], cb);
 }
 
 function doWatch(cb) {
-    gutil.log('update scripts');
-    pump([
-        gulp.src(bundle.BUNDLE),
-        babel({
-            presets: ['es2015']
-        }),
-        concat('bundle.js'),
-        gulp.dest('javascript/dist')
-    ], cb);
+	gutil.log('update scripts',bundle.BUNDLE);
+	pump([
+		gulp.src(bundle.BUNDLE),
+		babel({
+			presets: ['es2015']
+		}),
+		concat('bundle.js'),
+		gulp.dest('javascript/dist')
+	], cb);
 }
 
 function doSass(cb) {
-    gutil.log('update scss');
-    return gulp.src('./scss/**/*.scss')
-            .pipe(sass().on('error', sass.logError))
-            .pipe(gulp.dest('./css'));
+	gutil.log('update scss');
+	return gulp.src('./scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./css'));
 }
+
