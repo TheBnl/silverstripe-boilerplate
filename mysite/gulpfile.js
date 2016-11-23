dc03969e06df6f3f23c2eb19d5179826a238fae8
Bram de Leeuw <bram-de-leeuw@hotmail.com>
2016-11-23 12:03:01 +0100

    updated gulpfile

--- mysite/gulpfile.js
+++ mysite/gulpfile.js
@@ -5,54 +5,86 @@ const uglify = require('gulp-uglify');
 const pump = require('pump');
 const babel = require('gulp-babel');
 const bundle = require('./javascript/bundle.js');
-var sass = require('gulp-sass');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
-gulp.task('default', function(cb) {
-	return doBuild(cb);
+const config = {
+    browserSyncProxy: 'kobus.local',
+    jsDistFolder: 'javascript/dist',
+    jsDistFile: 'bundle.js'
+};
+
+const paths = {
+    js: 'javascript/app/**/*.js',
+    jsDist: 'javascript/dist',
+    sass: 'scss/**/*.scss',
+    css: 'css',
+    ss: 'templates/**/*.ss',
+};
+
+// default task falls back to watch
+gulp.task('default', ['watch']);
+
+// set up browser sync and watch for changes
+gulp.task('serve', ['setupSync', 'watch']);
+
+// compile and minify a distribution
+gulp.task('build', ['scripts', 'sass']);
+
+// watch fot changes and if browser sync is enabled reload/stream
+gulp.task('watch', ['scripts:quick', 'sass'], cb => {
+    gulp.watch(paths.js, ['scripts:quick']);
+    gulp.watch(paths.sass, ['sass']);
+    gulp.watch(paths.ss).on('change', browserSync.reload);
 });
 
-gulp.task('quickBuild', function (cb) {
-	return doWatch(cb);
+
+gulp.task('scripts:quick', cb => {
+    return doScripts(cb, true);
 });
 
-gulp.task('sass', function (cb) {
-	return doSass(cb);
+
+gulp.task('scripts', cb => {
+    return doScripts(cb, false);
 });
 
-gulp.task('watch', ['quickBuild','sass'], function(cb) {
-	gulp.watch('javascript/app/**/*.js', ['quickBuild']);
-	gulp.watch('scss/**/*.scss', ['sass']);
+
+gulp.task('sass', cb => {
+    return doSass(cb);
 });
 
-function doBuild(cb) {
-	gutil.log('compiling scripts',bundle.BUNDLE);
-	pump([
-		gulp.src(bundle.BUNDLE),
-		babel({
-			presets: ['es2015']
-		}),
-		concat('bundle.js'),
-		uglify(),
-		gulp.dest('javascript/dist')
-	], cb);
+
+gulp.task('setupSync', cb => {
+    browserSync.init({
+        proxy: config.browserSyncProxy
+    });
+});
+
+
+function doScripts(cb, quick) {
+    gutil.log('update js', quick ? 'fast' : '');
+    var buildPipes = [];
+    buildPipes.push(gulp.src(bundle.BUNDLE));
+    buildPipes.push(babel({
+        presets: ['es2015']
+    }));
+    buildPipes.push(concat(config.jsDistFile));
+
+    // if were doing a quick build skip uglify
+    if (!quick) buildPipes.push(uglify());
+
+    buildPipes.push(gulp.dest(config.jsDistFolder));
+    buildPipes.push(browserSync.stream());
+    pump(buildPipes, cb);
 }
 
-function doWatch(cb) {
-	gutil.log('update scripts',bundle.BUNDLE);
-	pump([
-		gulp.src(bundle.BUNDLE),
-		babel({
-			presets: ['es2015']
-		}),
-		concat('bundle.js'),
-		gulp.dest('javascript/dist')
-	], cb);
-}
 
 function doSass(cb) {
-	gutil.log('update scss');
-	return gulp.src('./scss/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./css'));
+    gutil.log('update scss');
+    pump([
+        gulp.src(paths.sass),
+        sass().on('error', sass.logError),
+        gulp.dest(paths.css),
+        browserSync.stream()
+    ], cb);
 }
-
