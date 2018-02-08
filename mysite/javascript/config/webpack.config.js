f4935b3798795c1df677985ce68a3b5826699b1a
Bram de Leeuw <bram-de-leeuw@hotmail.com>
2018-02-08 14:19:31 +0100

    add build context to config

--- mysite/javascript/config/webpack.config.js
+++ mysite/javascript/config/webpack.config.js
@@ -1,24 +1,44 @@
+const webpack = require('webpack');
+
 module.exports = {
+  config: {
     entry: {
-        app: './javascript/app/app.js'
+      app: './javascript/app/app.js'
     },
     output: {
-        filename: './javascript/dist/[name].bundle.js'
+      filename: './javascript/dist/[name].bundle.js'
     },
     resolve: {
-        modules: [
-            '../../node_modules'
-        ]
+      modules: [
+        '../../node_modules'
+      ]
     },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin({
+        name: "vendor"
+      })
+    ],
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader?presets[]=es2015'
-                }
+      rules: [
+        {
+          test: /\.js$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015'],
+              comments: false
             }
-        ]
+          }
+        }
+      ]
     }
+  },
+
+  get: function(build) {
+    if (build) {
+      this.config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+    }
+
+    return this.config;
+  }
 };
