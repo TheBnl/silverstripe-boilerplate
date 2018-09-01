b0f56c806a49963857e828be293e55bf1afccfa6
TheBnl <bram-de-leeuw@hotmail.com>
2018-09-01 15:24:13 +0200

    updated the package to include natives module

--- mysite/webpack.common.js
+++ mysite/webpack.common.js
@@ -3,34 +3,34 @@ const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: './javascript/app/app.js'
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'initial',
-      name: 'vendor'
-    }
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+    entry: {
+        app: './javascript/app/app.js'
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'initial',
+            name: 'vendor'
         }
-      }
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, 'javascript/dist'),
-    filename: '[name].bundle.js'
-  },
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/, /cookieconsent/],
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'javascript/dist'),
+        filename: '[name].bundle.js'
+    },
 };
