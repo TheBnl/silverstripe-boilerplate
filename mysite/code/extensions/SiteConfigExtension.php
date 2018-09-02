681b9cacf2794a64fb353dc7bf26a41dde57cb5f
TheBnl <bram-de-leeuw@hotmail.com>
2018-09-02 16:47:41 +0200

    reeanable addressable

--- mysite/code/extensions/SiteConfigExtension.php
+++ mysite/code/extensions/SiteConfigExtension.php
@@ -1,25 +1,28 @@
 <?php
 
+use SilverStripe\Forms\EmailField;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\GridField\GridField;
-use SilverStripe\Forms\LiteralField;
 use SilverStripe\Forms\TextField;
 use SilverStripe\ORM\DataExtension;
 
-
 /**
+ * class SiteConfigExtension
  * Extension to modify SiteConfig
- * @property SilverStripe\SiteConfig\SiteConfig owner
+ * @mixin \Symbiote\Addressable\Addressable
+ * @mixin \Symbiote\Addressable\Geocodable
+ *
+ * @property string Phone
+ * @property string Email
+ * @method \SilverStripe\ORM\HasManyList SocialMediaPlatforms
+ * @property SilverStripe\SiteConfig\SiteConfig|SiteConfigExtension owner
  */
 class SiteConfigExtension extends DataExtension
 {
-
     private static $db = array(
         'Phone' => 'Varchar(25)',
         'Email' => 'Varchar(255)'
     );
-
-    private static $has_one = array();
 
     private static $has_many = array(
         'SocialMediaPlatforms' => 'SocialMediaPlatform'
@@ -34,26 +37,23 @@ class SiteConfigExtension extends DataExtension
     public function updateCMSFields(FieldList $fields)
     {
         $fields->removeByName('Theme');
-
-        $telephoneField = new TextField('Phone', 'TelephoneNumber');
-        $emailField = new TextField('Email', 'Email');
-        $fields->addFieldsToTab("Root.Address", array($telephoneField, $emailField));
+        $fields->addFieldsToTab('Root.Address', [
+            TextField::create('Phone', 'TelephoneNumber'),
+            EmailField::create('Email', 'Email')
+        ]);
 
         if ($this->owner->exists()) {
             $editableGridFieldConfig = new GridFieldConfig_EditableNoDetail();
-            $socialMediaPlatformsGridField = new GridField("SocialMediaPlatforms", "Social Media Links", $this->owner->SocialMediaPlatforms(), $editableGridFieldConfig);
-
-            $fields->addFieldsToTab("Root.SocialMedia", array(
-                $socialMediaPlatformsGridField
+            $fields->addFieldToTab('Root.SocialMedia', GridField::create(
+                'SocialMediaPlatforms',
+                'Social Media Links',
+                $this->owner->SocialMediaPlatforms(),
+                $editableGridFieldConfig
             ));
-        } else {
-            $saveNeeded = _t('SiteConfig.SAVE_NEEDED', 'You need to save the Config before Social Media links can be added');
-            $fields->addFieldToTab('Root.SocialMediaLinks', new LiteralField('SaveNeeden', "<p class='message notice'>$saveNeeded</p>"));
         }
 
         return $fields;
     }
-
 
     /**
      * Return a formatted address
@@ -67,19 +67,5 @@ class SiteConfigExtension extends DataExtension
             $this->owner->getField('Postcode'),
             $this->owner->getField('Suburb')
         ));
-    }
-
-
-    /**
-     * Return a directional link to Google Maps
-     *
-     * @return string
-     */
-    public function getGoogleMapsLink()
-    {
-        $address = urlencode($this->owner->getFullAddress());
-        $latitude = $this->owner->getField("Latitude");
-        $longitude = $this->owner->getField("Longitude");
-        return "https://www.google.nl/maps/dir//$address/@$latitude,$longitude,16z/?hl=nl";
     }
 }
