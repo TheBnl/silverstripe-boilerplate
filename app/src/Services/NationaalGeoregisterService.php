8808d4ed00b0e68fc595c2b5f95e945549390fb7
Bram de Leeuw <bram@xd.nl>
2020-04-03 11:05:06 +0200

    add nationaal georegister as default address geocoder

---
+++ app/src/Services/NationaalGeoregisterService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace XD\Basic\Services;
+
+use Geocoder\Query\GeocodeQuery;
+use GuzzleHttp\Client as GuzzleClient;
+use Http\Adapter\Guzzle6\Client as HttpClient;
+use Swis\Geocoder\NationaalGeoregister\NationaalGeoregister;
+use Symbiote\Addressable\GeocodeServiceException;
+use Symbiote\Addressable\GeocodeServiceInterface;
+
+class NationaalGeoregisterService implements GeocodeServiceInterface
+{
+    /**
+     * Get the coordinates from the dutch Nationaal Georegister
+     *
+     * @param string $address
+     * @param string $region
+     *
+     * @return array
+     * @throws GeocodeServiceException
+     */
+    public function addressToPoint($address, $region = '')
+    {
+        $guzzle = new GuzzleClient([
+            'timeout' => 2.0,
+            'verify' => false,
+        ]);
+        $client  = new HttpClient($guzzle);
+        $geocoder = new NationaalGeoregister($client);
+        $query = GeocodeQuery::create($address);
+        $result = $geocoder->geocodeQuery($query);
+
+        if ($result->isEmpty()) {
+            throw new GeocodeServiceException('No result found', 0, null);
+        }
+
+        $location = $result->first()->getCoordinates();
+        return [
+            'lat' => (float)$location->getLatitude(),
+            'lng' => (float)$location->getLongitude()
+        ];
+    }
+}
