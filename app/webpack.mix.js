4d1936f9b7bedea60b04cf17d5b9ae24263e621d
Remy Vaartjes <remy@xd.nl>
2020-09-01 09:53:36 +0200

    Old version cannot build gives error
    Module not found: Error: Can't resolve 'core-js/modules/es.array.index-of'
    Solved by adding webpackConfig resolve.
    Also added jquery

--- app/webpack.mix.js
+++ app/webpack.mix.js
@@ -2,14 +2,24 @@ const mix = require('laravel-mix');
 require('laravel-mix-polyfill');
 
 mix
-  .setResourceRoot('/app/')
-  .js('client/src/js/app.js', 'client/dist/js')
-  .sass('client/src/styles/app.scss', 'client/dist/styles')
-  .sass('client/src/styles/cms.scss', 'client/dist/styles')
-  .sass('client/src/styles/editor.scss', 'client/dist/styles')
+    .autoload({
+      jquery: ['$', 'global.jQuery',"jQuery","global.$","jquery","global.jquery"]
+    })
+    .webpackConfig({
+      resolve: {
+        modules: [
+          path.resolve(__dirname, 'node_modules')
+        ]
+      }
+    })
+    .setResourceRoot('/app/')
+    .js('client/src/js/app.js', 'client/dist/js')
+    .sass('client/src/styles/app.scss', 'client/dist/styles')
+    .sass('client/src/styles/cms.scss', 'client/dist/styles')
+    .sass('client/src/styles/editor.scss', 'client/dist/styles')
   // .sourceMaps()
-  .polyfill({
-    enabled: mix.inProduction(),
-    useBuiltIns: "usage",
-    targets: {"ie": 11}
-  });
+    .polyfill({
+      enabled: mix.inProduction(),
+      useBuiltIns: "usage",
+      targets: {"ie": 11}
+    });
